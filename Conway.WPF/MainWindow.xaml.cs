2c5dfab16f7f82fc38693c4daaf9886ae8c7ef11
yalcin narin <narin_yalcin91@hotmail.com>
2021-10-11 23:49:03 +0200

    Zwaar verwerking !

--- Conway.WPF/MainWindow.xaml.cs
+++ Conway.WPF/MainWindow.xaml.cs
@@ -115,6 +115,8 @@ namespace Conway.WPF
 
         private void btn_Import_Click(object sender, RoutedEventArgs e)
         {
+            Clear();
+
             OpenFileDialog ofd = new OpenFileDialog();
             ofd.DefaultExt = ".ini";
             ofd.Filter = "Ini Document (.ini)|*.ini";
@@ -168,8 +170,8 @@ namespace Conway.WPF
                                     else { prix = "\u20AC" + _VolledigString[(j + 8)]; }
                                 }
                                 //test.Text = _VolledigString[900];
-                                //data_Ini.Items.Add(description);
                                 var data = new ModelIni { Id = id, Ean = ean, Description = description, Qt = qt, Prix = prix };
+                                ToMachine(id, description, prix);
                                 id++;
                                 data_Ini.Items.Add(data);
                             }
@@ -180,9 +182,149 @@ namespace Conway.WPF
             }
         }
 
-        public void ToMachine()
+        private void Clear()
         {
+            if (data_Ini != null)
+            {
+                data_Ini.Items.Clear();
+                lbl_1.Content = ""; lbl_1_Prix.Content = ""; lbl_2.Content = ""; lbl_2_Prix.Content = ""; lbl_3.Content = ""; lbl_3_Prix.Content = "";
+                lbl_4.Content = ""; lbl_4_Prix.Content = ""; lbl_5.Content = ""; lbl_5_Prix.Content = ""; lbl_6.Content = ""; lbl_6_Prix.Content = ""; lbl_7.Content = "";
+                lbl_7_Prix.Content = ""; lbl_8.Content = ""; lbl_8_Prix.Content = ""; lbl_9.Content = ""; lbl_9_Prix.Content = ""; lbl_10.Content = ""; lbl_10_Prix.Content = "";
 
+                lbl_11.Content = ""; lbl_11_Prix.Content = ""; lbl_12.Content = ""; lbl_12_Prix.Content = ""; lbl_13.Content = ""; lbl_13_Prix.Content = "";
+                lbl_14.Content = ""; lbl_14_Prix.Content = ""; lbl_15.Content = ""; lbl_15_Prix.Content = ""; lbl_16.Content = ""; lbl_16_Prix.Content = ""; lbl_17.Content = "";
+                lbl_17_Prix.Content = ""; lbl_18.Content = ""; lbl_18_Prix.Content = ""; lbl_19.Content = ""; lbl_19_Prix.Content = ""; lbl_20.Content = ""; lbl_20_Prix.Content = "";
+
+                lbl_21.Content = ""; lbl_21_Prix.Content = ""; lbl_22.Content = ""; lbl_22_Prix.Content = ""; lbl_23.Content = ""; lbl_23_Prix.Content = "";
+                lbl_24.Content = ""; lbl_24_Prix.Content = ""; lbl_25.Content = ""; lbl_25_Prix.Content = ""; lbl_26.Content = ""; lbl_26_Prix.Content = ""; lbl_27.Content = "";
+                lbl_27_Prix.Content = ""; lbl_28.Content = ""; lbl_28_Prix.Content = ""; lbl_29.Content = ""; lbl_29_Prix.Content = ""; lbl_30.Content = ""; lbl_30_Prix.Content = "";
+
+                lbl_31.Content = ""; lbl_31_Prix.Content = ""; lbl_32.Content = ""; lbl_32_Prix.Content = ""; lbl_33.Content = ""; lbl_33_Prix.Content = "";
+                lbl_34.Content = ""; lbl_34_Prix.Content = ""; lbl_35.Content = ""; lbl_35_Prix.Content = ""; lbl_36.Content = ""; lbl_36_Prix.Content = ""; lbl_37.Content = "";
+                lbl_37_Prix.Content = ""; lbl_38.Content = ""; lbl_38_Prix.Content = ""; lbl_39.Content = ""; lbl_39_Prix.Content = ""; lbl_40.Content = ""; lbl_40_Prix.Content = "";
+
+                lbl_41.Content = ""; lbl_41_Prix.Content = ""; lbl_42.Content = ""; lbl_42_Prix.Content = ""; lbl_43.Content = ""; lbl_43_Prix.Content = "";
+                lbl_44.Content = ""; lbl_44_Prix.Content = ""; lbl_45.Content = ""; lbl_45_Prix.Content = ""; lbl_46.Content = ""; lbl_46_Prix.Content = ""; lbl_47.Content = "";
+                lbl_47_Prix.Content = ""; lbl_48.Content = ""; lbl_48_Prix.Content = ""; lbl_49.Content = ""; lbl_49_Prix.Content = ""; lbl_50.Content = ""; lbl_50_Prix.Content = "";
+
+                lbl_51.Content = ""; lbl_51_Prix.Content = ""; lbl_52.Content = ""; lbl_52_Prix.Content = ""; lbl_53.Content = ""; lbl_53_Prix.Content = "";
+                lbl_54.Content = ""; lbl_54_Prix.Content = ""; lbl_55.Content = ""; lbl_55_Prix.Content = ""; lbl_56.Content = ""; lbl_56_Prix.Content = ""; lbl_57.Content = "";
+                lbl_57_Prix.Content = ""; lbl_58.Content = ""; lbl_58_Prix.Content = ""; lbl_59.Content = ""; lbl_59_Prix.Content = ""; lbl_60.Content = ""; lbl_60_Prix.Content = "";
+
+                lbl_61.Content = ""; lbl_61_Prix.Content = ""; lbl_62.Content = ""; lbl_62_Prix.Content = ""; lbl_63.Content = ""; lbl_63_Prix.Content = "";
+                lbl_64.Content = ""; lbl_64_Prix.Content = ""; lbl_65.Content = ""; lbl_65_Prix.Content = ""; lbl_66.Content = ""; lbl_66_Prix.Content = ""; lbl_67.Content = "";
+                lbl_67_Prix.Content = ""; lbl_68.Content = ""; lbl_68_Prix.Content = ""; lbl_69.Content = ""; lbl_69_Prix.Content = ""; lbl_70.Content = ""; lbl_70_Prix.Content = "";
+
+                lbl_71.Content = ""; lbl_71_Prix.Content = ""; lbl_72.Content = ""; lbl_72_Prix.Content = ""; lbl_73.Content = ""; lbl_73_Prix.Content = "";
+                lbl_74.Content = ""; lbl_74_Prix.Content = ""; lbl_75.Content = ""; lbl_75_Prix.Content = ""; lbl_76.Content = ""; lbl_76_Prix.Content = ""; lbl_77.Content = "";
+                lbl_77_Prix.Content = ""; lbl_78.Content = ""; lbl_78_Prix.Content = ""; lbl_79.Content = ""; lbl_79_Prix.Content = ""; lbl_80.Content = ""; lbl_80_Prix.Content = "";
+
+                lbl_81.Content = ""; lbl_81_Prix.Content = ""; lbl_82.Content = ""; lbl_82_Prix.Content = ""; lbl_83.Content = ""; lbl_83_Prix.Content = "";
+                lbl_84.Content = ""; lbl_84_Prix.Content = ""; lbl_85.Content = ""; lbl_85_Prix.Content = ""; lbl_86.Content = ""; lbl_86_Prix.Content = ""; lbl_87.Content = "";
+                lbl_87_Prix.Content = ""; lbl_88.Content = ""; lbl_88_Prix.Content = ""; lbl_89.Content = ""; lbl_89_Prix.Content = ""; lbl_90.Content = ""; lbl_90_Prix.Content = "";
+            }
+        }
+
+        private void ToMachine(int i, string description, string prix)
+        {
+            if (i == 1) { lbl_1.Content = description; lbl_1_Prix.Content = prix; }
+            if (i == 2) { lbl_2.Content = description; lbl_2_Prix.Content = prix; }
+            if (i == 3) { lbl_3.Content = description; lbl_3_Prix.Content = prix; }
+            if (i == 4) { lbl_4.Content = description; lbl_4_Prix.Content = prix; }
+            if (i == 5) { lbl_5.Content = description; lbl_5_Prix.Content = prix; }
+            if (i == 6) { lbl_6.Content = description; lbl_6_Prix.Content = prix; }
+            if (i == 7) { lbl_7.Content = description; lbl_7_Prix.Content = prix; }
+            if (i == 8) { lbl_8.Content = description; lbl_8_Prix.Content = prix; }
+            if (i == 9) { lbl_9.Content = description; lbl_9_Prix.Content = prix; }
+            if (i == 10) { lbl_10.Content = description; lbl_10_Prix.Content = prix; }
+
+            if (i == 11) { lbl_11.Content = description; lbl_11_Prix.Content = prix; }
+            if (i == 12) { lbl_12.Content = description; lbl_12_Prix.Content = prix; }
+            if (i == 13) { lbl_13.Content = description; lbl_13_Prix.Content = prix; }
+            if (i == 14) { lbl_14.Content = description; lbl_14_Prix.Content = prix; }
+            if (i == 15) { lbl_15.Content = description; lbl_15_Prix.Content = prix; }
+            if (i == 16) { lbl_16.Content = description; lbl_16_Prix.Content = prix; }
+            if (i == 17) { lbl_17.Content = description; lbl_17_Prix.Content = prix; }
+            if (i == 18) { lbl_18.Content = description; lbl_18_Prix.Content = prix; }
+            if (i == 19) { lbl_19.Content = description; lbl_19_Prix.Content = prix; }
+            if (i == 20) { lbl_20.Content = description; lbl_20_Prix.Content = prix; }
+
+            if (i == 21) { lbl_21.Content = description; lbl_21_Prix.Content = prix; }
+            if (i == 22) { lbl_22.Content = description; lbl_22_Prix.Content = prix; }
+            if (i == 23) { lbl_23.Content = description; lbl_23_Prix.Content = prix; }
+            if (i == 24) { lbl_24.Content = description; lbl_24_Prix.Content = prix; }
+            if (i == 25) { lbl_25.Content = description; lbl_25_Prix.Content = prix; }
+            if (i == 26) { lbl_26.Content = description; lbl_26_Prix.Content = prix; }
+            if (i == 27) { lbl_27.Content = description; lbl_27_Prix.Content = prix; }
+            if (i == 28) { lbl_28.Content = description; lbl_28_Prix.Content = prix; }
+            if (i == 29) { lbl_29.Content = description; lbl_29_Prix.Content = prix; }
+            if (i == 30) { lbl_30.Content = description; lbl_30_Prix.Content = prix; }
+
+            if (i == 31) { lbl_31.Content = description; lbl_31_Prix.Content = prix; }
+            if (i == 32) { lbl_32.Content = description; lbl_32_Prix.Content = prix; }
+            if (i == 33) { lbl_33.Content = description; lbl_33_Prix.Content = prix; }
+            if (i == 34) { lbl_34.Content = description; lbl_34_Prix.Content = prix; }
+            if (i == 35) { lbl_35.Content = description; lbl_35_Prix.Content = prix; }
+            if (i == 36) { lbl_36.Content = description; lbl_36_Prix.Content = prix; }
+            if (i == 37) { lbl_37.Content = description; lbl_37_Prix.Content = prix; }
+            if (i == 38) { lbl_38.Content = description; lbl_38_Prix.Content = prix; }
+            if (i == 39) { lbl_39.Content = description; lbl_39_Prix.Content = prix; }
+            if (i == 40) { lbl_40.Content = description; lbl_40_Prix.Content = prix; }
+
+            if (i == 41) { lbl_41.Content = description; lbl_41_Prix.Content = prix; }
+            if (i == 42) { lbl_42.Content = description; lbl_42_Prix.Content = prix; }
+            if (i == 43) { lbl_43.Content = description; lbl_43_Prix.Content = prix; }
+            if (i == 44) { lbl_44.Content = description; lbl_44_Prix.Content = prix; }
+            if (i == 45) { lbl_45.Content = description; lbl_45_Prix.Content = prix; }
+            if (i == 46) { lbl_46.Content = description; lbl_46_Prix.Content = prix; }
+            if (i == 47) { lbl_47.Content = description; lbl_47_Prix.Content = prix; }
+            if (i == 48) { lbl_48.Content = description; lbl_48_Prix.Content = prix; }
+            if (i == 49) { lbl_49.Content = description; lbl_49_Prix.Content = prix; }
+            if (i == 50) { lbl_50.Content = description; lbl_50_Prix.Content = prix; }
+
+            if (i == 51) { lbl_51.Content = description; lbl_51_Prix.Content = prix; }
+            if (i == 52) { lbl_52.Content = description; lbl_52_Prix.Content = prix; }
+            if (i == 53) { lbl_53.Content = description; lbl_53_Prix.Content = prix; }
+            if (i == 54) { lbl_54.Content = description; lbl_54_Prix.Content = prix; }
+            if (i == 55) { lbl_55.Content = description; lbl_55_Prix.Content = prix; }
+            if (i == 56) { lbl_56.Content = description; lbl_56_Prix.Content = prix; }
+            if (i == 57) { lbl_57.Content = description; lbl_57_Prix.Content = prix; }
+            if (i == 58) { lbl_58.Content = description; lbl_58_Prix.Content = prix; }
+            if (i == 59) { lbl_59.Content = description; lbl_59_Prix.Content = prix; }
+            if (i == 60) { lbl_60.Content = description; lbl_60_Prix.Content = prix; }
+
+            if (i == 61) { lbl_61.Content = description; lbl_61_Prix.Content = prix; }
+            if (i == 62) { lbl_62.Content = description; lbl_62_Prix.Content = prix; }
+            if (i == 63) { lbl_63.Content = description; lbl_63_Prix.Content = prix; }
+            if (i == 64) { lbl_64.Content = description; lbl_64_Prix.Content = prix; }
+            if (i == 65) { lbl_65.Content = description; lbl_65_Prix.Content = prix; }
+            if (i == 66) { lbl_66.Content = description; lbl_66_Prix.Content = prix; }
+            if (i == 67) { lbl_67.Content = description; lbl_67_Prix.Content = prix; }
+            if (i == 68) { lbl_68.Content = description; lbl_68_Prix.Content = prix; }
+            if (i == 69) { lbl_69.Content = description; lbl_69_Prix.Content = prix; }
+            if (i == 70) { lbl_70.Content = description; lbl_70_Prix.Content = prix; }
+
+            if (i == 71) { lbl_71.Content = description; lbl_71_Prix.Content = prix; }
+            if (i == 72) { lbl_72.Content = description; lbl_72_Prix.Content = prix; }
+            if (i == 73) { lbl_73.Content = description; lbl_73_Prix.Content = prix; }
+            if (i == 74) { lbl_74.Content = description; lbl_74_Prix.Content = prix; }
+            if (i == 75) { lbl_75.Content = description; lbl_75_Prix.Content = prix; }
+            if (i == 76) { lbl_76.Content = description; lbl_76_Prix.Content = prix; }
+            if (i == 77) { lbl_77.Content = description; lbl_77_Prix.Content = prix; }
+            if (i == 78) { lbl_78.Content = description; lbl_78_Prix.Content = prix; }
+            if (i == 79) { lbl_79.Content = description; lbl_79_Prix.Content = prix; }
+            if (i == 80) { lbl_80.Content = description; lbl_80_Prix.Content = prix; }
+
+            if (i == 81) { lbl_81.Content = description; lbl_81_Prix.Content = prix; }
+            if(i == 82) { lbl_82.Content = description; lbl_82_Prix.Content = prix; }
+            if(i == 83) { lbl_83.Content = description; lbl_83_Prix.Content = prix; }
+            if(i == 84) { lbl_84.Content = description; lbl_84_Prix.Content = prix; }
+            if(i == 85) { lbl_85.Content = description; lbl_85_Prix.Content = prix; }
+            if(i == 86) { lbl_86.Content = description; lbl_86_Prix.Content = prix; }
+            if(i == 87) { lbl_87.Content = description; lbl_87_Prix.Content = prix; }
+            if(i == 88) { lbl_88.Content = description; lbl_88_Prix.Content = prix; }
+            if(i == 89) { lbl_89.Content = description; lbl_89_Prix.Content = prix; }
+            if(i == 90) { lbl_90.Content = description; lbl_90_Prix.Content = prix; }
         }
 
         public class ModelIni
